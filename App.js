c4129e7d1512d149a7783a13fb7460fb2e59c688
Ryan Liszewski <ryan@voltacharging.com>
2018-07-05 15:29:44 -0700

    Map is displayed with markers in SF

--- App.js
+++ App.js
@@ -4,15 +4,101 @@ import Mapbox from '@mapbox/react-native-mapbox-gl';
 
 Mapbox.setAccessToken("pk.eyJ1Ijoicnlhbmxpc3pld3NraSIsImEiOiJjamo5MHQ3bzEwYWtwM3BsaDJuNGZubXYxIn0.lk66Ohmpd3j8s945fi01jA")
 
+const coordinates = [
+  [
+    -122.40181,
+    37.768249
+  ],
+  [
+    -122.40181,
+    37.768249
+  ],
+  [
+    -122.458705,
+    37.763935
+  ],
+  [
+    -122.4767259,
+    37.7283771
+  ],
+  [
+    -122.4766695,
+    37.7283728
+  ],
+  [
+    -122.393775,
+    37.768299
+  ],
+  [
+    -122.3922545,
+    37.765967
+  ],
+  [
+    -122.47598965,
+    37.7266635
+  ],
+  [
+    -122.3897115,
+    37.768046
+  ],
+  [
+    -122.476339666667,
+    37.727876
+  ],
+  [
+    -122.4494492,
+    37.8060998
+  ],
+]
+
 export default class App extends Component {
+  
+  constructor(props){
+    super(props)
+
+    this.state = {
+      coordinates: coordinates,
+    }
+  }
+
+  renderMarker = (index) => {
+    const id = `pointMarker${index}`
+    const coordinate = this.state.coordinates[index]
+
+
+    return(
+      <Mapbox.PointAnnotation
+        key={id}
+        id={id}
+        coordinate={coordinate}
+      >
+      <View style={styles.markerContainer}>
+          <View style={styles.markerFill} />
+      </View>
+
+      </Mapbox.PointAnnotation>
+    )
+  }
+
+  renderMarkers = () => {
+    const markers = [];
+
+    for(i = 0; i < this.state.coordinates.length; i++){
+      markers.push(this.renderMarker(i))
+    }
+    return markers
+  }
+
   render() {
     return (
       <View style={styles.container}>
          <Mapbox.MapView
             styleURL={Mapbox.StyleURL.Street}
-            zoomLevel={15}
+            zoomLevel={10}
             centerCoordinate={[-122.4194, 37.7749]}
             style={styles.container}>
+
+            { this.renderMarkers() }
         </Mapbox.MapView>
       </View>
     );
@@ -24,14 +110,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  markerFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor:'black',
+    transform: [{ scale: 0.4 }],
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  markerContainer: {
+    width: 20,
+    height: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#9B9B9B',
+    borderRadius: 10,
   },
 });
