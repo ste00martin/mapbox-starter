1a1afb44460d115bd371ea5222c7b78dc948fc42
Stefan Martin <ste00martin@gmail.com>
2018-07-12 12:57:16 -0700

    getting android to build then crash

--- App.js
+++ App.js
@@ -1,8 +1,34 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import Mapbox from '@mapbox/react-native-mapbox-gl';
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
 
-Mapbox.setAccessToken("pk.eyJ1Ijoicnlhbmxpc3pld3NraSIsImEiOiJjamo5MHQ3bzEwYWtwM3BsaDJuNGZubXYxIn0.lk66Ohmpd3j8s945fi01jA")
+export const IS_ANDROID = Platform.OS === 'android';
+
+MapboxGL.setAccessToken("pk.eyJ1Ijoicnlhbmxpc3pld3NraSIsImEiOiJjamo5MHQ3bzEwYWtwM3BsaDJuNGZubXYxIn0.lk66Ohmpd3j8s945fi01jA")
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+
+  markerFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor:'black',
+    transform: [{ scale: 0.4 }],
+  },
+
+  markerContainer: {
+    width: 20,
+    height: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#9B9B9B',
+    borderRadius: 10,
+  },
+});
 
 const coordinates = [
   [
@@ -52,79 +78,85 @@ const coordinates = [
 ]
 
 export default class App extends Component {
-  
+
+
   constructor(props){
     super(props)
 
     this.state = {
       coordinates: coordinates,
+      isAndroidPermissionGranted: false,
+      isFetchingAndroidPermission: IS_ANDROID,
     }
   }
 
-  renderMarker = (index) => {
-    const id = `pointMarker${index}`
-    const coordinate = this.state.coordinates[index]
-
-
-    return(
-      <Mapbox.PointAnnotation
-        key={id}
-        id={id}
-        coordinate={coordinate}
-      >
-      <View style={styles.markerContainer}>
-          <View style={styles.markerFill} />
-      </View>
-
-      </Mapbox.PointAnnotation>
-    )
-  }
-
-  renderMarkers = () => {
-    const markers = [];
-
-    for(i = 0; i < this.state.coordinates.length; i++){
-      markers.push(this.renderMarker(i))
+  async componentWillMount() {
+    if (IS_ANDROID) {
+      console.log('heyo')
+      // const isGranted = await MapboxGL.requestAndroidLocationPermissions();
+      // this.setState({
+      //   isAndroidPermissionGranted: isGranted,
+      //   isFetchingAndroidPermission: false,
+      // });
     }
-    return markers
   }
+
+
+  // renderMarker = (index) => {
+  //   const id = `pointMarker${index}`
+  //   const coordinate = this.state.coordinates[index]
+
+
+  //   return(
+  //     <MapboxGL.PointAnnotation
+  //       key={id}
+  //       id={id}
+  //       coordinate={coordinate}
+  //     >
+  //     <View style={styles.markerContainer}>
+  //         <View style={styles.markerFill} />
+  //     </View>
+
+  //     </MapboxGL.PointAnnotation>
+  //   )
+  // }
+
+  // renderMarkers = () => {
+  //   const markers = [];
+
+  //   for(i = 0; i < this.state.coordinates.length; i++){
+  //     markers.push(this.renderMarker(i))
+  //   }
+  //   return markers
+  // }
 
   render() {
+    if (IS_ANDROID && !this.state.isAndroidPermissionGranted) {
+      if (this.state.isFetchingAndroidPermission) {
+        return null;
+      }
+      return (
+        <View style={sheet.matchParent}>
+          <Text style={styles.noPermissionsText}>
+            You need to accept location permissions in order to use this example
+            applications
+          </Text>
+        </View>
+      );
+    }
+
     return (
       <View style={styles.container}>
-         <Mapbox.MapView
+         {/* <Mapbox.MapView
             styleURL={Mapbox.StyleURL.Street}
             zoomLevel={10}
             centerCoordinate={[-122.4194, 37.7749]}
             style={styles.container}>
 
             { this.renderMarkers() }
-        </Mapbox.MapView>
+        </Mapbox.MapView> */}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-  },
-
-  markerFill: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    backgroundColor:'black',
-    transform: [{ scale: 0.4 }],
-  },
-
-  markerContainer: {
-    width: 20,
-    height: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#9B9B9B',
-    borderRadius: 10,
-  },
-});
--- android/app/src/main/java/com/mapboxstarter/MainApplication.java
+++ android/app/src/main/java/com/mapboxstarter/MainApplication.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.mapbox.rctmgl.RCTMGLPackage;
+// import com.facebook.appevents.AppEventsLogger;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RCTMGLPackage()
       );
     }
 
@@ -40,6 +43,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    // AppEventsLogger.activateApp(this);
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
